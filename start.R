235e9c08674b242ea45a9d4c83559b47edf69577
Remko Duursma <remkoduursma@gmail.com>
2019-02-04 14:45:32 +0100

    bootstrap nearly integrated

--- start.R
+++ start.R
@@ -2,7 +2,4 @@ library(bookdown)
 
 # see _output.yml for settings
 bookdown::render_book('index.Rmd', 'bookdown::gitbook')
-
-
-# ... eventuall
 bookdown::render_book('index.Rmd', 'bookdown::pdf_book', clean=FALSE)
