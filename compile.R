f5376822547746f973cd89a45f0da3708b2d729d
Remko Duursma <remkoduursma@gmail.com>
2019-02-08 09:49:56 +0100

    Removed text data files; added draft mixed effects; added programming; compiles OK

--- compile.R
+++ compile.R
@@ -1,4 +1,4 @@
 # see _output.yml for settings
-bookdown::render_book('index.Rmd', 'bookdown::gitbook')
-#bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
+bookdown::render_book('index.Rmd', 'bookdown::gitbook', new_session=TRUE, clean=TRUE)
+#bookdown::render_book('index.Rmd', 'bookdown::pdf_book', new_session=TRUE, clean=TRUE)
 #-->does not work in same session at the moment, still conflict Hmisc/dplyr
