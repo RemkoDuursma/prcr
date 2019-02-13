62766b3d954a742feba398af50fbeca9cf842fec
Remko Duursma <remkoduursma@gmail.com>
2019-02-13 12:17:52 +0100

    style and font changed; HTML and PDF compile in one session

--- compile.R
+++ compile.R
@@ -1,4 +1,18 @@
-# see _output.yml for settings
+# Render book as HTML and PDF
+# Important input files:
+#   _output.yml:  settings
+#   css/style.css: for formatting, including fonts
+#   index.Rmd: includes a block with knitr settings
+#   preamble.tex: required packages and settings for LaTeX (PDF only)
+# Output:
+#  _book/index.html (and link to PDF from there)
 bookdown::render_book('index.Rmd', 'bookdown::gitbook', new_session=TRUE, clean=TRUE)
-#bookdown::render_book('index.Rmd', 'bookdown::pdf_book', new_session=TRUE, clean=TRUE)
-#-->does not work in same session at the moment, still conflict Hmisc/dplyr
+bookdown::render_book('index.Rmd', 'bookdown::pdf_book', new_session=TRUE, clean=TRUE)
+
+
+# Quick command to re-render some of the document.
+# Do this when you are adjusting the CSS, for example
+# bookdown::render_book(input=c('index.Rmd',"01-intro.Rmd"),
+#                       'bookdown::gitbook', 
+#                       preview=TRUE,
+#                       new_session=TRUE, clean=TRUE)
