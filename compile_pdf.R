c753cc8de183cfd4f0b5ad9fb648d66013af3806
Remko Duursma <remkoduursma [at] gmail.com>
2020-02-25 23:08:39 +0100

    programming tips

--- compile_pdf.R
+++ compile_pdf.R
@@ -1,1 +1,4 @@
-bookdown::render_book(input="rmd/index.Rmd", 'bookdown::pdf_book', new_session=TRUE, clean=FALSE)
+bookdown::render_book(input = "rmd/index.Rmd", 
+                      output_format = "bookdown::pdf_book", 
+                      new_session = TRUE, 
+                      clean = FALSE)
